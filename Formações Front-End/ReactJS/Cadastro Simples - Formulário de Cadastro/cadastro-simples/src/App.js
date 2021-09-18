1038eddfbef18e522134b2ca3c6165bf1c21f9d7
Rafael-Araujo-dev <web.dev@orafaelribeiro.com>
2021-09-18 14:18:38 -0300

    Updated user interface with Material UI

--- Formações Front-End/ReactJS/Cadastro Simples - Formulário de Cadastro/cadastro-simples/src/App.js
+++ Formações Front-End/ReactJS/Cadastro Simples - Formulário de Cadastro/cadastro-simples/src/App.js
@@ -1,16 +1,28 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 
 import './App.css';
 import FormularioCadastro from './components/FormularioCadastro/Formulario';
+
+import { Container, Typography } from '@material-ui/core';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 export default class App extends Component {
 
   render() {
     return (
-      <Fragment>
-        <h1>Formulário de Cadastro</h1>
+      <Container 
+        component="article"
+        maxWidth="sm"
+      >
+        <Typography 
+          variant="h3"
+          component="h1"
+        >Formulário de Cadastro</Typography>
         <FormularioCadastro />
-      </Fragment>
+      </Container>
     );
   }
 }
--- Formações Front-End/ReactJS/Cadastro Simples - Formulário de Cadastro/cadastro-simples/src/components/FormularioCadastro/Formulario.jsx
+++ Formações Front-End/ReactJS/Cadastro Simples - Formulário de Cadastro/cadastro-simples/src/components/FormularioCadastro/Formulario.jsx
@@ -1,24 +1,55 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
+import {Button, TextField, Switch, FormControlLabel} from '@material-ui/core/';
 
 const FormularioCadastro = () => {
     return (
         <>
             <form>
-                <label>Nome</label>
-                <input type="text"/>
+                <TextField
+                    id="nome"
+                    label="Nome"
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                />
 
-                <label>Sobrenome</label>
-                <input type="text"/>
+                <TextField
+                    id="sobrenome"
+                    label="Sobrenome"
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                />
 
-                <label>CPF</label>
-                <input type="text"/>
+               <TextField
+                    id="cpf"
+                    label="CPF"
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                />
 
-                <label>Promoções</label>
-                <input type="checkbox"/>
-
-                <label>Novidades</label>
-                <input type="checkbox"/>
+                <FormControlLabel
+                    label="Promoções"
+                    control={
+                        <Switch
+                            name="promocoes"
+                            defaultChecked
+                            color="primary"
+                        />
+                    } 
+                />
+                
+                <FormControlLabel
+                    label="Novidades"
+                    control={
+                        <Switch
+                            name="novidades"
+                            defaultChecked
+                            color="primary"
+                        />
+                    }
+                />
 
                 <Button 
                     type="submit" 
